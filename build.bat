a85bbdedae8e93c007cd7467a0edb2865adc9bda
Jason Kleban <jkleban@outlook.com>
2018-08-21 04:33:54 -0400

    The #rust channel says I need to target aarch64 and that I need a aarch64 linker

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 SET RUST_BACKTRACE=1
-xargo rustc --target arm-raspi3-none-elf --release --verbose -- --emit=obj
+xargo rustc --target aarch64-raspi3-none-elf --release --verbose -- --emit=obj --emit=llvm-ir
